ebd6d738c3418f197b7abd3294b717deee78ee84
ayushman <unixparadoxfb@gmail.com>
2022-03-15 14:54:06 +0545

    day 2

--- src/main/scala/leapYear.scala
+++ src/main/scala/leapYear.scala
@@ -18,25 +18,47 @@ object leapYear extends App {
     case (n1,n2) => s"one for $n1, one for $n2"
     case _: Any => s"one for you, one for me"
   }
-  print(returnSentence)
+  //print(returnSentence)
 
 
   //finding age of one person in 4 planets
   val userAgeInSeconds = 12312423
   class calculateAge(val ageInSeconds: Double) {
-    def calculateAgeInEveryPlanet(): Unit = {
+
+    def calculateAgeInEveryPlanet(): List[Double] = {
       val earthDays = 31557600
       val earthAge = (ageInSeconds / earthDays)
       val mercuryAge = (ageInSeconds / earthDays) * 0.2408467
       val venusAge = (ageInSeconds / earthDays) * 0.61519726
       val marsAge = (ageInSeconds / earthDays) * 1.8808158
       val x = List(earthAge, mercuryAge, venusAge, marsAge)
-      println(x)
+      x
     }
   }
-
   val Dipak = new calculateAge(315576003)
   val y = Dipak.calculateAgeInEveryPlanet()
-  //println(y)
+  println(y)
+
+
+  class Bob {
+    def statementType(s: String): Unit = {
+      val response = s match {
+        case s1 if s.isEmpty => "Fine. Be that way!"
+        case s1 if shouting(s) => "Calm down, I know what i'm doing"
+        case s1 if question(s) => "Sure."
+        case _ => "Whatever."
+      }
+      print(response)
+    }
+
+    def shouting(s: String): Boolean = s.toUpperCase() == s
+    def question(s: String): Boolean = s.takeRight(1) == '?'
+
+  }
+
+  val bob = new Bob
+  bob.statementType("what up?")
+
+
 
 }
